125f26e42a3bb22272e5d1037d26a7b60c5e2a4c
Anton <2603049@gmail.com>
2019-03-20 11:18:54 +0300

    добавил эффект ховер для секции layouts

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
+    var count = 0;
+    var target = $('#map');
+    var targetPos = target.offset().top;
+    var winHeight = $(window).height();
+    var scrollToMap = targetPos - winHeight;
+    $(window).scroll(function () {
+        var winScrollTop = $(this).scrollTop();
+        if(winScrollTop > scrollToMap & count == 0) {
+            target.append('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9f5c91b84eb6c3ff696b5b9913842fee6b26918a6edb60f932c440bf38ae371f&amp;width=100%25&amp;height=418&amp;lang=ru_RU&amp;scroll=false"></script>');
+            count++;
+        }
+    });
+
+
     $(".owl-carousel").owlCarousel({
         loop: true,
         margin: 32.2,
